55c151deffec4cc0fa20a22881947b95f73208e5
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2018-04-20 16:24:26 -0400

    Fix visibility of patched Kernel#require method.

--- lib/autoload_reloader/core_ext/kernel_require.rb
+++ lib/autoload_reloader/core_ext/kernel_require.rb
@@ -1,5 +1,7 @@
 module AutoloadReloader # to access it's private constants
   module ::Kernel
+    module_function
+
     alias_method :require_without_autoload_reloader, :require
     def require(path)
       Autoloads.around_require(path) do
